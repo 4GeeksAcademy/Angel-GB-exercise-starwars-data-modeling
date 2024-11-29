2c07b99558dfb488438f45cc03c1a51a8b87aa28
Angel Gallego <angelgb1990@gmail.com>
2024-11-29 20:38:16 +0000

    primer commit

--- src/models.py
+++ src/models.py
@@ -7,26 +7,109 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class User(Base):
+    __tablename__ = 'user'
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_name = Column(String(20), nullable=False, unique=True)
+    password = Column(String(20), nullable=False)
+    name = Column(String(20), nullable=False)
+    last_name = Column(String(20), nullable=False)
+    email = Column(String(50) , nullable=False, unique=True)
+
+class Character(Base):
+    __tablename__ = 'character'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    height = Column(String(50), nullable=False)
+    mass = Column(String(50), nullable=False)
+    hair_color = Column(String(50), nullable=False)
+    skin_color = Column(String(50), nullable=False)
+    eye_color = Column(String(50), nullable=False)
+    birth_year = Column(String(50), nullable=False)
+    gender = Column(String(50), nullable=False)
+    gender = Column(String(50), nullable=False)
+    homeworld = relationship('Planet', back_populates='residents')
+    homeworld_id = Column(Integer, ForeignKey('planet.id'))
+
+class Planet(Base):
+    __tablename__ = 'planet'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    diameter = Column(String(50), nullable=False)
+    rotation_period = Column(String(50), nullable=False)
+    orbital_period = Column(String(50), nullable=False)
+    gravity = Column(String(50), nullable=False)
+    population = Column(String(50), nullable=False)
+    climate = Column(String(50), nullable=False)
+    terrain = Column(String(50), nullable=False)
+    surface_water = Column(String(50), nullable=False)
+    character_id = Column(String(50), nullable=False)
+
+class Species(Base):
+    __tablename__ = 'species'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    classification = Column(String(50), nullable=False)
+    designation = Column(String(50), nullable=False)
+    average_height = Column(String(50), nullable=False)
+    average_lifespan = Column(String(50), nullable=False)
+    hair_colors = Column(String(50), nullable=False)
+    skin_colors = Column(String(50), nullable=False)
+    eye_colors = Column(String(50), nullable=False)
+    language = Column(String(50), nullable=False)
+    description = Column(String(50), nullable=False)
+
+class Starships(Base):
+    __tablename__ = 'starships'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    model = Column(String(50), nullable=False)
+    starship_class = Column(String(50), nullable=False)
+    manufacturer = Column(String(50), nullable=False)
+    cost_in_credits = Column(String(50), nullable=False)
+    length = Column(String(50), nullable=False)
+    crew = Column(String(50), nullable=False)
+    passengers = Column(String(50), nullable=False)
+    max_atmosphering_speed = Column(String(50), nullable=False)
+    hyperdrive_rating = Column(String(50), nullable=False)
+    MGLT = Column(String(50), nullable=False)
+    cargo_capacity = Column(String(50), nullable=False)
+    description = Column(String(50), nullable=False)
+
+class Vehicles(Base):
+    __tablename__ = 'vehicles'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    model = Column(String(50), nullable=False)
+    vehicle_class = Column(String(50), nullable=False)
+    manufacturer = Column(String(50), nullable=False)
+    cost_in_credits = Column(String(50), nullable=False)
+    length = Column(String(50), nullable=False)
+    crew = Column(String(50), nullable=False)
+    passengers = Column(String(50), nullable=False)
+    max_atmosphering_speed = Column(String(50), nullable=False)
+    cargo_capacity = Column(String(50), nullable=False)
+    consumables = Column(String(50), nullable=False)
+    description = Column(String(50), nullable=False)
+
+# class Address(Base):
+#     __tablename__ = 'address'
+#     id = Column(Integer, primary_key=True)
+#     street_name = Column(String(250))
+#     street_number = Column(String(250))
+#     post_code = Column(String(250), nullable=False)
+#     person_id = Column(Integer, ForeignKey('person.id'))
+#     person = relationship(Person)
 
     def to_dict(self):
         return {}
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
+# Address_id = Column(Integer, ForeignKey('address_id'))
