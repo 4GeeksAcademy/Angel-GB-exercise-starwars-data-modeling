2fb31df9fb73d3b33497ef9fa0d715655d96ded6
Angel Gallego <angelgb1990@gmail.com>
2024-12-09 19:47:21 +0000

    terminado v2

--- src/models.py
+++ src/models.py
@@ -15,7 +15,9 @@ class User(Base):
     password = Column(String(20), nullable=False)
     name = Column(String(20), nullable=False)
     last_name = Column(String(20), nullable=False)
-    email = Column(String(50) , nullable=False, unique=True)
+    email = Column(String(50), nullable=False, unique=True)
+
+    favorites = relationship('Favorite', back_populates='user')
 
 class Character(Base):
     __tablename__ = 'character'
@@ -29,10 +31,17 @@ class Character(Base):
     eye_color = Column(String(50), nullable=False)
     birth_year = Column(String(50), nullable=False)
     gender = Column(String(50), nullable=False)
-    gender = Column(String(50), nullable=False)
-    homeworld = relationship('Planet', back_populates='residents')
-    homeworld_id = Column(Integer, ForeignKey('planet.id'))
 
+
+    specie_id = Column(Integer, ForeignKey('specie.id'))
+    specie = relationship('Specie', back_populates='characters')
+
+    homeworld_id = Column(Integer, ForeignKey('planet.id'))
+    homeworld = relationship('Planet', back_populates='residents')
+
+    favorites = relationship('Favorite', back_populates='character')
+
+# Tabla 'Planet'
 class Planet(Base):
     __tablename__ = 'planet'
 
@@ -46,10 +55,14 @@ class Planet(Base):
     climate = Column(String(50), nullable=False)
     terrain = Column(String(50), nullable=False)
     surface_water = Column(String(50), nullable=False)
-    character_id = Column(String(50), nullable=False)
 
-class Species(Base):
-    __tablename__ = 'species'
+    residents = relationship('Character', back_populates='homeworld')
+
+    favorites = relationship('Favorite', back_populates='planet')
+
+# Tabla 'Specie'
+class Specie(Base):
+    __tablename__ = 'specie'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(50), nullable=False)
@@ -61,10 +74,14 @@ class Species(Base):
     skin_colors = Column(String(50), nullable=False)
     eye_colors = Column(String(50), nullable=False)
     language = Column(String(50), nullable=False)
-    description = Column(String(50), nullable=False)
+    description = Column(String(250), nullable=False)
 
-class Starships(Base):
-    __tablename__ = 'starships'
+    characters = relationship('Character', back_populates='specie')
+
+    favorites = relationship('Favorite', back_populates='specie')
+
+class Starship(Base):
+    __tablename__ = 'starship'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(50), nullable=False)
@@ -79,10 +96,12 @@ class Starships(Base):
     hyperdrive_rating = Column(String(50), nullable=False)
     MGLT = Column(String(50), nullable=False)
     cargo_capacity = Column(String(50), nullable=False)
-    description = Column(String(50), nullable=False)
+    description = Column(String(250), nullable=False)
 
-class Vehicles(Base):
-    __tablename__ = 'vehicles'
+    favorites = relationship('Favorite', back_populates='starship')
+
+class Vehicle(Base):
+    __tablename__ = 'vehicle'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(50), nullable=False)
@@ -96,20 +115,29 @@ class Vehicles(Base):
     max_atmosphering_speed = Column(String(50), nullable=False)
     cargo_capacity = Column(String(50), nullable=False)
     consumables = Column(String(50), nullable=False)
-    description = Column(String(50), nullable=False)
+    description = Column(String(250), nullable=False)
 
-# class Address(Base):
-#     __tablename__ = 'address'
-#     id = Column(Integer, primary_key=True)
-#     street_name = Column(String(250))
-#     street_number = Column(String(250))
-#     post_code = Column(String(250), nullable=False)
-#     person_id = Column(Integer, ForeignKey('person.id'))
-#     person = relationship(Person)
+    favorites = relationship('Favorite', back_populates='vehicle')
+
+class Favorite(Base):
+    __tablename__ = 'favorite'
+    
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    character_id = Column(Integer, ForeignKey('character.id'), nullable=True)
+    vehicle_id = Column(Integer, ForeignKey('vehicle.id'), nullable=True)
+    starship_id = Column(Integer, ForeignKey('starship.id'), nullable=True)
+    planet_id = Column(Integer, ForeignKey('planet.id'), nullable=True)
+    specie_id = Column(Integer, ForeignKey('specie.id'), nullable=True)
+
+    user = relationship('User', back_populates='favorites')
+    character = relationship('Character', back_populates='favorites')
+    vehicle = relationship('Vehicle', back_populates='favorites')
+    starship = relationship('Starship', back_populates='favorites')
+    planet = relationship('Planet', back_populates='favorites')
+    specie = relationship('Specie', back_populates='favorites')
 
     def to_dict(self):
         return {}
 
-## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
-# Address_id = Column(Integer, ForeignKey('address_id'))
